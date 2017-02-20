ad97b96e3514d9c1cc74094efb4f3c4459ab44f3
Dave Hulse <davehulse@MacBook-Pro.local>
2017-02-20 10:35:30 -0800

    text sizing fixes

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 // Modified, but originally from:
 // https://stackoverflow.com/questions/21961839/simulation-background-size-cover-in-canvas/21961894#21961894
 function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
-	
+
    EXIF.getData(img, function() {
-	   
+
        if (arguments.length === 2) {
            x = y = 0;
            w = ctx.canvas.width;
@@ -28,8 +28,8 @@ function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
            nh = ih * r,   // new prop. height
            cx, cy, cw, ch, ar = 1;
 
-       // decide which gap to fill    
-       if (nw < w) ar = w / nw;                             
+       // decide which gap to fill
+       if (nw < w) ar = w / nw;
        if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
        nw *= ar;
        nh *= ar;
@@ -48,7 +48,7 @@ function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
        if (ch > ih) ch = ih;
 
 		var orientation = EXIF.getTag(this, "Orientation");
-		
+
 		switch(orientation){
 
        	case 8:
@@ -81,9 +81,9 @@ function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
 
    	 }
 
-	});	
+	});
 
-   
+
 }
 
 if(!window.File || !window.FileReader) alert
@@ -99,7 +99,7 @@ var jsMeme =
 {
   canvas: null,
   cache: null,
-  
+
   init: function()
   {
     jsMeme.canvas = $('.canvas-container > canvas').get(0);
@@ -113,52 +113,54 @@ var jsMeme =
         jsMeme.captions.redraw();
       });
     });
-    
+
     $('input[name="maximum-width"]').typingEnd(function()
     {
       var mw = parseFloat(this.value);
           if(mw < 200 || mw > 4096) return false;
-      
+
       jsMeme.file.maxWidth = mw;
-      
+
       var oWidth = jsMeme.canvas.width,
           oHeight = jsMeme.canvas.height,
           nWidth = jsMeme.file.maxWidth,
           nHeight = Math.round(nWidth / oWidth * oHeight);
-      
+
       jsMeme.canvas.width = nWidth;
       jsMeme.canvas.height = nHeight;
       jsMeme.canvas.getContext('2d').drawImage(jsMeme.cache, 0, 0, oWidth, oHeight, 0, 0, nWidth, nHeight);
-      
+
       jsMeme.cache.width = nWidth;
       jsMeme.cache.height = nHeight;
       jsMeme.cache.getContext('2d').drawImage(jsMeme.canvas, 0, 0);
 
     });
-    
+
     // $('body > aside > h1').append(' <b>' + $('ul.changelog li[v]').first().attr('v') + '</b>');
   },
-  
+
   reset: function()
   {
     window.location = window.location.href;
     var canvas_calc = $("canvas").width() / 2
   },
-  
+
   captions:
   {
     list:
     {
       'line-1':
       {
-        x: 5, y: 101,
+        x: 5,
+				y: 109,
         alignment: 'left',
         font: "500 20px \"MarkOff\", Arial, sans-serif",
         text: "",
       },
       'line-2':
       {
-        x: 5, y: 87,
+        x: 5,
+				y: 90,
         alignment: 'left',
         font: "500 20px \"MarkOff\", Arial, sans-serif",
         text: ""
@@ -166,13 +168,13 @@ var jsMeme =
       'line-3':
       {
         x: 5,
-        y: 75,
+        y: 78,
         alignment: 'left',
         font: "500 20px \"MarkOff\", Arial, sans-serif",
         text: ""
       }
     },
-    
+
     layout: function(layout)
     {
       switch(layout)
@@ -182,13 +184,13 @@ var jsMeme =
           jsMeme.captions.list['line-1'].alignment = 'center';
           jsMeme.captions.list['line-2'].alignment = 'center';
           break;
-          
+
         case 'classic':
           jsMeme.captions.list['line-1'].y = -50;
           jsMeme.captions.list['line-1'].alignment = 'center';
           jsMeme.captions.list['line-2'].alignment = 'center';
           break;
-        
+
         default:
           jsMeme.captions.list['line-1'].y = 20;
           jsMeme.captions.list['line-1'].alignment = 'left';
@@ -198,11 +200,11 @@ var jsMeme =
       }
       jsMeme.captions.redraw();
     },
-    
+
     redraw: function()
     {
       jsMeme.canvas.getContext('2d').drawImage(jsMeme.cache, 0, 0);
-      
+
       for(i in jsMeme.captions.list)
         jsMeme.text(
           jsMeme.captions.list[i].text,
@@ -214,11 +216,11 @@ var jsMeme =
         );
     }
   },
-  
+
   text: function(val, x, y, alignment, font, shadowstrength)
   {
     var ctx = jsMeme.canvas.getContext('2d');
-        ctx.font = "500 200% \"MarkOff\", Arial, sans-serif";
+        ctx.font = "500 240% \"MarkOff\", Arial, sans-serif";
         ctx.textAlign = "start";
         ctx.textBaseline = "top";
 
@@ -232,7 +234,7 @@ var jsMeme =
       y = jsMeme.canvas.height + y;
       ctx.textBaseline = "bottom";
     }
-    
+
     if(alignment == 'center'){
       x = Math.round((jsMeme.canvas.width / 2) - (ctx.measureText(val).width / 2));
     }else if(alignment == 'right'){
@@ -243,13 +245,13 @@ var jsMeme =
     // ctx.shadowOffsetX = 0;
     // ctx.shadowOffsetY = 1;
     // ctx.shadowBlur = 5;
-    
+
     // for(i = 0; i < shadowstrength; i ++)
     // {
     //   ctx.fillStyle = "#000";
     //   ctx.fillText(val, x, y);
     // }
-    
+
     ctx.fillStyle = "#FFFFFF";
 
     // console.log('This is x test:', x);
@@ -257,15 +259,15 @@ var jsMeme =
     var one = canvas_calc * 0.25 / x
     var two = canvas_calc * 55 / y
     // font = "500 3% \"MarkOff\", Arial, sans-serif";
-    
 
-    console.log(one); 
+
+    console.log(one);
     // var two = y / 7
 
 
     //This is pulling in px instead of percent. How native function works
     ctx.fillText(val, one, two);
-  
+
   var xhr = new XMLHttpRequest();
 
   xhr.open("GET", "/images/white_logo_copy.png");
@@ -281,10 +283,10 @@ var jsMeme =
 
        /*    if($(window).width() <= 768 ){
           //Rotates image
-          jsMeme[lo[op]].getContext('2d').translate(canvas_width /1,canvas_width /900) 
+          jsMeme[lo[op]].getContext('2d').translate(canvas_width /1,canvas_width /900)
           jsMeme[lo[op]].getContext('2d').rotate(90 * Math.PI / 180);
-          
- 
+
+
         }*/
           // jsMeme.canvas.getContext('2d').drawImage(img, 30, 30, 254, 36)
       }
@@ -292,13 +294,13 @@ var jsMeme =
     }
     xhr.send();
   },
-  
+
   file:
   {
     current: {},
-    
+
     maxWidth: 500,
-    
+
     render: function(source)
     {
       $('body > main:not(.ready)').addClass('ready');
@@ -318,14 +320,14 @@ var jsMeme =
         //Check so rotates only on mobile 768px
         // if($(window).width() <= 768 ){
         //   //Rotates image
-        //   jsMeme[lo[op]].getContext('2d').translate(canvas_width /1,canvas_width /900) 
+        //   jsMeme[lo[op]].getContext('2d').translate(canvas_width /1,canvas_width /900)
         //   jsMeme[lo[op]].getContext('2d').rotate(90 * Math.PI / 180);
-          
- 
+
+
         // }
 
 //         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-//  jsMeme[lo[op]].getContext('2d').translate(canvas_width /1,canvas_width /900) 
+//  jsMeme[lo[op]].getContext('2d').translate(canvas_width /1,canvas_width /900)
 //           jsMeme[lo[op]].getContext('2d').rotate(90 * Math.PI / 180);
 // }
 
@@ -335,12 +337,12 @@ var jsMeme =
         var wrh = jsMeme[lo[op]].width / jsMeme[lo[op]].height;
         var newWidth = jsMeme[lo[op]].width;
         var newHeight = newWidth / wrh;
-		
+
 		drawImageProp(jsMeme[lo[op]].getContext('2d'), source, 0, 0, canvas_width, canvas_height);
 
         //jsMeme[lo[op]].getContext('2d').drawImage(source, 0,0, newWidth , newHeight);
       }
-    
+
       var xhr = new XMLHttpRequest();
       var canvas_calc = $("canvas").width() / 2;
       xhr.open("GET", "/images/white_logo_copy.png");
@@ -356,9 +358,9 @@ var jsMeme =
 
       }
       xhr.send();
-  
+
     },
-    
+
     import: function(input)
     {
       $('body > main').addClass('ready');
@@ -368,7 +370,7 @@ var jsMeme =
           {
             var file = input.files[i];
                 jsMeme.file.current = file;
-                
+
             if(file.name.match(/\.psd$/i))
             {
               PSD.fromFile(file, function(psd)
@@ -380,13 +382,13 @@ var jsMeme =
                     jsMeme.file.render(this);
                     $(this).remove();
                   };
-                
+
                 $(psdimg).prependTo('body');
               });
             }
             else
             {
-				
+
               var reader = new FileReader();
                 reader.onload = function(e)
                 {
@@ -396,13 +398,13 @@ var jsMeme =
                     {
                       jsMeme.file.render(this);
                       $(this).remove();
-					  
-					  
+
+
                     };
 
                   $(imgimg).prependTo('body');
                 };
-				
+
 				// reader.onloadend = function() {
 //
 // 					var imgimg = $('#foo');
@@ -410,15 +412,15 @@ var jsMeme =
 //
 //
 // 				};
-				
+
                 reader.readAsDataURL(file);
             }
-			
-            
+
+
             return !1;
           }
     },
-    
+
     export: function()
     {
       jsMeme.canvas.toBlob(function(blob)
@@ -426,28 +428,28 @@ var jsMeme =
         var filename = jsMeme.file.current.name
           .replace(/\.(.*?)$/, '-GlobalCitzen-ThisisAmerica.png')
           .replace(/\{date\}/gi, new Date().toUTCString().toLowerCase().replace(/[^a-zA-Z0-9]/g, '-'));
-        
+
         if(filename.indexOf('.png') < 0)
           filename += '.png';
-        
+
         saveAs(blob, filename);
       }, 'image/png');
     }
   },
-  
+
   webcam:
   {
     start: function()
     {
       if($('body > aside > .webcam').is(':visible'))
         return jsMeme.webcam.stop();
-      
+
       $('body > aside > .drop').hide();
       $('body > aside > .webcam').show();
-      
+
       var video = $('body > aside > .webcam > video')[0],
           streaming = !1, width = 220, height = 0;
-      
+
       navigator.getMedia = (
         navigator.getUserMedia ||
         navigator.webkitGetUserMedia ||
@@ -485,19 +487,19 @@ var jsMeme =
         }
       }, !1);
     },
-    
+
     stop: function()
     {
       $('body > aside > .drop').show();
       $('body > aside > .webcam').hide();
       $('body > aside > .webcam > video')[0].pause();
     },
-    
+
     reset: function()
     {
       $('body > aside > .webcam > video')[0].play();
     },
-    
+
     take: function()
     {
       var video = $('body > aside > .webcam > video')[0],
@@ -505,19 +507,19 @@ var jsMeme =
           oHeight = video.height,
           width = video.videoWidth,
           height = video.videoHeight;
-      
+
       video.width = width;
       video.height = height;
       video.pause();
-      
+
       var tmp = document.createElement('canvas');
           tmp.width = width;
           tmp.height = height;
           tmp.getContext('2d').drawImage(video, 0, 0);
-      
+
       jsMeme.file.current = { name: "webcam-{date}" };
       jsMeme.file.render(tmp);
-      
+
       video.width = oWidth;
       video.height = oHeight;
     }
@@ -526,7 +528,7 @@ var jsMeme =
 
 $(jsMeme.init);
 
-function updateText(type) { 
+function updateText(type) {
  var id = "third-input";
  document.getElementById(id).value = document.getElementById(type).value;
   document.getElementById("third-input").focus();
@@ -541,7 +543,7 @@ function updateText(type) {
 
 // if($)
 // if(document.getElementById('sensor').value == "Refugee.") {
-//    
+//
 //     }
 
 $('.get_started').click(function() {
@@ -611,7 +613,7 @@ $('.close').click(function() {
 
 
 
-    
+
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  $('.move-down').css("transform", "translate(0px, 125px)");
  $('.drag-title').empty();
@@ -624,10 +626,10 @@ $('.close').click(function() {
 
 // $(".img_1-2").on('touchmove','.scrollable',function(e) {
 //     e.stopPropagation();
-// }); 
+// });
 
 if(navigator.userAgent.match(/iPhone/)) {
-  
+
 
     $(".dropdown").addClass("dropdown-mobile");
 }
