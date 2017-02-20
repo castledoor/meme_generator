8555ae6bbfd972a0f36421263999219428c3d8dc
Rob Derstadt <rderstadt@gmail.com>
2017-02-20 09:41:57 -0700

    Fixed image aspect ratio

--- js/index.js
+++ js/index.js
@@ -1,4 +1,51 @@
+// From https://stackoverflow.com/questions/21961839/simulation-background-size-cover-in-canvas/21961894#21961894
+function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
 
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
+
+    // default offset is center
+    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    var iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih),
+        nw = iw * r,   // new prop. width
+        nh = ih * r,   // new prop. height
+        cx, cy, cw, ch, ar = 1;
+
+    // decide which gap to fill    
+    if (nw < w) ar = w / nw;                             
+    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
+    nw *= ar;
+    nh *= ar;
+
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+}
 
 if(!window.File || !window.FileReader) alert
 (
@@ -219,7 +266,7 @@ var jsMeme =
       $('canvas').css('width', "100%");
       var a = "http://www.google.com/intl/en_com/images/logo_plain.png";
       var canvas_width = $('.canvas-container' ).width();
-      
+      var canvas_height = $('.canvas-container' ).height();
       var lo = ['canvas', 'cache'],
           width = source.width,
           height = source.width,
@@ -249,8 +296,10 @@ var jsMeme =
         var wrh = jsMeme[lo[op]].width / jsMeme[lo[op]].height;
         var newWidth = jsMeme[lo[op]].width;
         var newHeight = newWidth / wrh;
+		
+		drawImageProp(jsMeme[lo[op]].getContext('2d'), source, 0, 0, canvas_width, canvas_height);
 
-        jsMeme[lo[op]].getContext('2d').drawImage(source, 0,0, newWidth , newHeight);
+        //jsMeme[lo[op]].getContext('2d').drawImage(source, 0,0, newWidth , newHeight);
       }
     
       var xhr = new XMLHttpRequest();
